9bfce9dacf2f88b402b49b95eb5a83f010d77385
h.samadi <samadi.hadis@gmail.com>
2023-11-03 15:20:40 +0330

    Add binding to Web Page Activity

--- app/src/main/java/com/hadis/newsapplication/WebPageActivity.kt
+++ app/src/main/java/com/hadis/newsapplication/WebPageActivity.kt
@@ -2,13 +2,14 @@ package com.hadis.newsapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.webkit.WebView
+import com.hadis.newsapplication.databinding.ActivityWebPageBinding
 
 class WebPageActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWebPageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_web_page)
-        val webView = findViewById<WebView>(R.id.webView)
-        webView.loadUrl(intent.getStringExtra("url")!!)
+        binding = ActivityWebPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.webView.loadUrl(intent.getStringExtra("url").orEmpty())
     }
 }
